56ddcdb7acf1c0d6a1ac500e8b56e7491bfec187
Joanna Wang <joannaahj@gmail.com>
2024-01-14 16:21:36 -0600

    chore: extend eslint-plugin-jsx-a11y (#2)

--- ui/.eslintrc.cjs
+++ ui/.eslintrc.cjs
@@ -1,7 +1,7 @@
 /* eslint-env node */
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:jsx-a11y/recommended'],
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
+    plugins: ['@typescript-eslint', 'jsx-a11y'],
     root: true,
   };
