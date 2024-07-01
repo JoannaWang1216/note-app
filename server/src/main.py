65c18eaf408147a20678bf513b552962f0adf84b
Joanna Wang <joannaahj@gmail.com>
2024-06-30 19:46:39 -0500

    feat: add testing using in memory database

--- server/src/main.py
+++ server/src/main.py
@@ -1,5 +1,4 @@
-from typing import Annotated, MutableSequence
-from uuid import uuid4
+from typing import Annotated
 
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
@@ -25,60 +24,69 @@ app.add_middleware(
 SQLModel.metadata.create_all(engine)
 
 
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
 class NoteWrite(BaseModel):
     title: str
     content: str
 
 
 @app.get("/api/notes")
-async def get_notes():
-    with Session(engine) as session:
-        notes = session.exec(
-            select(models.Note).order_by(
-                models.Note.created_at.desc()  # pylint: disable=no-member
-            )
-        ).all()
-        return notes
+async def get_notes(session: Annotated[Session, Depends(get_session)]):
+    notes = session.exec(
+        select(models.Note).order_by(
+            models.Note.created_at.desc()  # pylint: disable=no-member # type: ignore
+        )
+    ).all()
+    return notes
 
 
 @app.get("/api/notes/{uuid}")
-async def get_note(uuid: UUID4):
-    with Session(engine) as session:
-        note = session.exec(select(models.Note).where(models.Note.id == uuid)).first()
-        if note is None:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND, detail="Item not found"
-            )
-        return note
+async def get_note(uuid: UUID4, session: Annotated[Session, Depends(get_session)]):
+    note = session.exec(select(models.Note).where(models.Note.id == uuid)).first()
+    if note is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Item not found"
+        )
+    return note
 
 
 @app.post("/api/notes")
-async def add_note(note: NoteWrite):
+async def add_note(note: NoteWrite, session: Annotated[Session, Depends(get_session)]):
     new_note = models.Note(title=note.title, content=note.content)
-    with Session(engine) as session:
-        session.add(new_note)
-        session.commit()
-    return new_note
+    session.add(new_note)
+    copied_note = new_note.model_copy()
+    session.commit()
+    return copied_note
 
 
 @app.put("/api/notes/{uuid}", dependencies=[Depends(get_note)])
-async def update_note(uuid: UUID4, note_write: NoteWrite):
-    with Session(engine) as session:
-        note = session.exec(select(models.Note).where(models.Note.id == uuid)).one()
-        note.title = note_write.title
-        note.content = note_write.content
-        session.add(note)
-        session.commit()
-        session.refresh(note)
-        return note
+async def update_note(
+    uuid: UUID4,
+    note_write: NoteWrite,
+    session: Annotated[Session, Depends(get_session)],
+):
+    note = session.exec(select(models.Note).where(models.Note.id == uuid)).one()
+    note.title = note_write.title
+    note.content = note_write.content
+    session.add(note)
+    session.commit()
+    session.refresh(note)
+    return note
 
 
 @app.delete("/api/notes/{uuid}")
-async def delete_note(uuid: UUID4, note: Annotated[models.Note, Depends(get_note)]):
-    with Session(engine) as session:
-        note_to_delete = session.exec(
-            select(models.Note).where(models.Note.id == uuid)
-        ).one()
-        session.delete(note_to_delete)
-        session.commit()
+async def delete_note(
+    uuid: UUID4,
+    session: Annotated[Session, Depends(get_session)],
+    note: Annotated[models.Note, Depends(get_note)],
+):
+    note_to_delete = session.exec(
+        select(models.Note).where(models.Note.id == uuid)
+    ).one()
+    session.delete(note_to_delete)
+    session.commit()
     return note
